9ae1cbf7459d4af99be388604cc80f17ec7a7b1c
Shardul Negi <shardul.negi21@gmail.com>
2018-08-10 03:15:46 -0400

    Update CardSection.js

--- src/components/CardSection.js
+++ src/components/CardSection.js
@@ -2,8 +2,7 @@ import React from 'react';
 import { View } from 'react-native';
 
 
-const CardSection =(props) => {
-
+const CardSection = (props) => {
   return (
     <View style={styles.containerStyle}>
     {props.children}
